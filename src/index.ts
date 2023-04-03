a367e56609b1845cfac4dd85d7f0adaae1194066
sjtommylee <sjtommylee@gmail.com>
2023-04-02 21:19:04 -0700

    chore: commit save point

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// https://deno.land/std@0.156.0/http/server.ts
+import { serve } from "https://deno.land/std@0.156.0/http/server.ts";
+import { handler } from "../build/deno_rust_wasm.js";
+
+serve(handler);
